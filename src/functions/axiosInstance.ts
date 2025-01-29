97d3257cb3c49cadee59ad6124783dd3c7e02d38
Abdo Ahmed <ba2662003@gmail.com>
2025-01-30 01:47:37 +0200

    updates

--- src/functions/axiosInstance.ts
+++ src/functions/axiosInstance.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_BACEND_API
 export const axiosInstance = axios.create({
     baseURL:`${apiUrl}/api/v1`,
-    withCredentials:true
+    // withCredentials:true
 })

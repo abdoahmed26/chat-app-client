0ddaf150b4bd7530ed29705bdf72912b7ab9ff88
Abdo Ahmed <ba2662003@gmail.com>
2025-02-01 00:20:44 +0200

    add backend api

--- src/functions/axiosInstance.ts
+++ src/functions/axiosInstance.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_BACEND_API
 export const axiosInstance = axios.create({
     baseURL:`${apiUrl}/api/v1`,
-    // withCredentials:true
+    withCredentials:true,
+    maxRedirects: 0,
 })
